76ca21ced0a6f1f38226dd986c50cde31b89fd9a
Thaddeus <thaddeusleezx@gmail.com>
2023-08-27 01:21:15 +0800

    Initial commit

---
+++ src/main/java/com/truecoloursgame/truecoloursgame/TrueColoursGameApplication.java
@@ -0,0 +1,13 @@
+package com.truecoloursgame.truecoloursgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrueColoursGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrueColoursGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/truecoloursgame/truecoloursgame/TrueColoursGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truecoloursgame.truecoloursgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrueColoursGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
