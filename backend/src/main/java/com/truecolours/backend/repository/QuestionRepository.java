2044fb7c834f84ede0de464b30217964c7a6f3b0
Thaddeus <thaddeusleezx@gmail.com>
2024-12-24 19:32:32 +0800

    feat: fetch all questions on room creation

--- backend/src/main/java/com/truecolours/backend/repository/QuestionRepository.java
+++ backend/src/main/java/com/truecolours/backend/repository/QuestionRepository.java
@@ -1,15 +1,15 @@
 package com.truecolours.backend.repository;
 
-import com.truecolours.backend.entity.Question;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
+import com.truecolours.backend.entity.Question;
 
 public interface QuestionRepository extends CrudRepository<Question, Integer> {
     @Query(nativeQuery = true, value = """
             SELECT * FROM question
-            ORDER BY RAND()
-            LIMIT 20;""")
+            ORDER BY RAND();""")
     List<Question> getRandomQuestions();
 }
