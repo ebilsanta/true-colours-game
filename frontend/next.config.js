3be98c2c1100b70d4fea3787e1273cbd282bdb46
Thaddeus <thaddeusleezx@gmail.com>
2026-06-05 23:05:15 +0800

    Update next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,17 @@
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true
-  }
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
